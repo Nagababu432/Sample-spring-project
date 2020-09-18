5cc0d8663805405b5c98ec65eb69a5e4b839bcc8
nagababu <kondraguntaa9@gmail.com>
2020-09-18 12:49:28 +0530

    first commit

---
+++ Downloads/springboot.first.app (1)/springboot.first.app/src/main/java/com/springboot1/app/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.springboot1.app;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+ 
+
+@RestController
+public class WelcomeController{
+	
+	@GetMapping("/welcome")
+	public  String welcome() {
+		return "welcome to spring boot app development";
+	}
+
+}
